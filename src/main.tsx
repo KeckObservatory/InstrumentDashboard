d56ba68cf1ae560e19d4a8a488a463ac98f1b68b
Tyler Tucker <tylertucker202@gmail.com>
2026-01-13 11:47:22 -1000

    handling browser routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = createRoot(
